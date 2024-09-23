13d88a294787d4966cedc6321338857adb6a1b37
B FAROOQ <bfarooq2022@gmail.com>
2024-09-23 15:47:26 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+//mongoose url handy 
+// mongodb+srv://farooqytworld:MCmsI1ZfOMxGYAB3@cluster0.wrgm2.mongodb.net/
+mongoose.connect("mongodb+srv://farooqytworld:MCmsI1ZfOMxGYAB3@cluster0.wrgm2.mongodb.net/todos")
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos', todoSchema);
+module.export = {
+    todo
+}
